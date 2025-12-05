f5231e8e1aa74be6f5dda7ea9df9dbb8e239144a
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-12-06 03:13:58 +0530

    updated with downgrading the prisma

--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,18 @@
 const express = require("express");
 const axios = require("axios");
+const { PrismaClient } = require("@prisma/client");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+const prisma = new PrismaClient();
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
+
+
+console.log(CLIENT_ID)
+console.log(CLIENT_SECRET)
+console.log(REDIRECT_URI)
